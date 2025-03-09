3398fa7ba8f1f69d311bcdb127a848b57927dab9
Okamitah <179641048+Okamitah@users.noreply.github.com>
2025-03-09 18:14:02 +0000

    Added threading and some basic routing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpStream, TcpListener};
 use std::io::{Read, Write};
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").expect("Can't listene on this port :'(");
@@ -7,7 +8,11 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_client(stream),
+            Ok(stream) => {
+                thread::spawn(|| {            
+                     handle_client(stream);
+                });
+            },
             Err(e) => eprintln!("Connection failed: {}", e),
         }
     }
@@ -22,9 +27,12 @@ fn handle_client(mut stream: TcpStream) {
             let request = String::from_utf8_lossy(&buffer);
             println!("Received request:\n{}", request);
             if let Some((method, path)) = parse_request(&request) {
-                println!("Method: {}, Path: {}", method, path);
+                let response = match (method, path) {
+                    ("GET", "/") => "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!",
+                    ("GET", "/about") => "HTTP/1.1 200 OK\r\nContent-Length: 18\r\n\r\nAbout this server",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
             
-                let response = "HTTP/1.1 200 OK\r\n\r\nHey there!";
                 stream.write_all(response.as_bytes()).expect("Couldn't hear you :(");
             } else {
                 let response = "HTTP/1.1 400 Bad Request\r\n\r\n";
